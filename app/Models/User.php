189b6617fc4005fe70c9ffb2e6164910ed9ba595
Dandy-Candra <106506074+Dandy-Candra@users.noreply.github.com>
2025-01-10 21:29:35 +0700

    add Panel model

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,7 @@ use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Filament\Panel;
 
 class User extends Authenticatable implements FilamentUser
 {
